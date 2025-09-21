e60d68da4f0ec642c930ba25e910f3e608cfc75c
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-09-21 12:50:44 +0600

    add task form ui completed

---
+++ src/components/tasks/AddTaskModal.jsx
@@ -0,0 +1,89 @@
+import { useForm } from "react-hook-form";
+import Modal from "../ui/Modal";
+
+const AddTaskModal = ({ isOpen, setIsOpen }) => {
+  const { register, handleSubmit, reset } = useForm();
+
+  const onCancel = () => {
+    reset();
+    setIsOpen(false);
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
+    onCancel();
+  };
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="I am using headlessui">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col gap-3">
+          <label htmlFor="title">Title</label>
+          <input
+            className="w-full rounded-md"
+            type="text"
+            id="title"
+            {...register("title")}
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <label htmlFor="description">Description</label>
+          <textarea
+            className="w-full rounded-md"
+            type="text"
+            id="description"
+            {...register("description")}
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <label htmlFor="deadline">Deadline</label>
+          <input
+            className="w-full rounded-md"
+            type="date"
+            id="deadline"
+            {...register("deadline")}
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <label htmlFor="assignTo">Asign to</label>
+          <select
+            className="w-full rounded-md"
+            type="text"
+            id="asignTo"
+            {...register("asignTo")}
+          >
+            <option value="Rahim">Rahim</option>
+            <option value="Karim">Karim</option>
+          </select>
+        </div>
+        <div className="flex flex-col gap-3">
+          <label htmlFor="priority">Priority</label>
+          <select
+            className="w-full rounded-md"
+            type="text"
+            id="priority"
+            {...register("priority")}
+          >
+            <option value="High">High</option>
+            <option value="Medium">Medium</option>
+            <option value="Low">Low</option>
+          </select>
+        </div>
+
+        <div className="flex items-center justify-end gap-3 mt-4">
+          <button
+            className="px-4 py-2 bg-red-300 rounded-lg"
+            onClick={() => onCancel()}
+            type="button"
+          >
+            Cancel
+          </button>
+          <button className="px-4 py-2 bg-green-300 rounded-lg" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </Modal>
+  );
+};
+
+export default AddTaskModal;
---
+++ src/components/ui/Modal.jsx
@@ -0,0 +1,33 @@
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+
+export default function Modal({ isOpen, setIsOpen, title, children }) {
+  function close() {
+    setIsOpen(false);
+  }
+
+  return (
+    <>
+      <Dialog
+        open={isOpen}
+        as="div"
+        className="relative z-10 focus:outline-none"
+        onClose={close}
+        __demoMode
+      >
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4">
+            <DialogPanel
+              transition
+              className="w-full max-w-md rounded-xl p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
+            >
+              <DialogTitle as="h3" className="text-lg font-semibold">
+                {title}
+              </DialogTitle>
+              <div>{children}</div>
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
+    </>
+  );
+}
