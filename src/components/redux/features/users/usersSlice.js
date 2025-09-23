1131cb55d73cb9810efe644498e47efdd0cd960c
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-09-23 22:59:21 +0600

    created usersSlice

---
+++ src/components/redux/features/users/usersSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const usersSlice =createSlice({
+    name: "usersSlice",
+    initialState,
+    reducers:{},
+})
+
+export default usersSlice.reducer
