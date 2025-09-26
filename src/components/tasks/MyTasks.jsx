759bd0e95d1cedf05070a6b2e07c8e2f546c6577
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-09-26 20:20:47 +0600

    show task details from myTasks details button

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -2,17 +2,24 @@ import {
   CheckIcon,
   DocumentMagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 import { setUserTask } from "../redux/features/tasks/tasksSlice";
+import TaskDetails from "./TaskDetails";
 
 const MyTasks = () => {
   const { tasks, userTasks } = useSelector((state) => state.tasksSlice);
   const { name: userName } = useSelector((state) => state.usersSlice);
-  console.log(userName);
+  const [isOpen, setIsOpen] = useState(false);
+  const [taskId, setTaskId] = useState(0);
 
   const dispatch = useDispatch();
+
+  const handleModal = (id) => {
+    setTaskId(id);
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     dispatch(setUserTask(userName));
@@ -20,6 +27,11 @@ const MyTasks = () => {
 
   return (
     <div>
+      <TaskDetails
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        id={taskId}
+      ></TaskDetails>
       <h1 className="text-xl my-3">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
         {userTasks?.map((item) => (
@@ -29,7 +41,11 @@ const MyTasks = () => {
           >
             <h1>{item.title}</h1>
             <div className="flex gap-3">
-              <button className="grid place-content-center" title="Details">
+              <button
+                onClick={() => handleModal(item.id)}
+                className="grid place-content-center cursor-pointer"
+                title="Details"
+              >
                 <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
               </button>
               <button className="grid place-content-center" title="Done">
--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -10,15 +10,15 @@ export default function Modal({ isOpen, setIsOpen, title, children }) {
       <Dialog
         open={isOpen}
         as="div"
-        className="relative z-10 focus:outline-none"
+        className="relative z-10 focus:outline-none "
         onClose={close}
         __demoMode
       >
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4">
+          <div className="flex min-h-full items-center justify-center p-4 ">
             <DialogPanel
               transition
-              className="w-full max-w-md rounded-xl p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
+              className="w-full max-w-md rounded-xl p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0 bg-slate-200"
             >
               <DialogTitle as="h3" className="text-lg font-semibold">
                 {title}
